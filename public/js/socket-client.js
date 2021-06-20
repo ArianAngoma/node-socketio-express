35083b32aa38a5bdde9184fe6956a2f6f095cc86
unknown <arian.angoma@tecsup.edu.pe>
2021-06-20 04:51:41 -0500

    Socket.io - node - express

---
+++ public/js/socket-client.js
@@ -0,0 +1,38 @@
+// Referencias del HTML
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+const txtMsg = document.querySelector('#txtMsg');
+const btnSend = document.querySelector('#btnSend');
+
+const socket = io();
+
+socket.on('connect', () => {
+    // console.log('Conectado')
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+});
+
+socket.on('disconnect', () => {
+    console.log('Desconectado');
+
+    lblOnline.style.display = 'none';
+    lblOffline.style.display = '';
+});
+
+// Resive el payload enviado desde el servidor
+socket.on('send-msg', (payload) => {
+    console.log(payload);
+})
+
+btnSend.addEventListener('click', () => {
+    const msg = txtMsg.value;
+
+    // Envia el mensaje (msg) hacia el servidor
+    /* socket.emit('send-msg', msg); */
+
+    // Envia el mensaje hacia el servidor para que se muestre solo al mismo cliente
+    socket.emit('send-msg', msg, (id) => {
+        console.log('Desde el server', id);
+    });
+
+})
---
+++ sockets/controller.js
@@ -0,0 +1,23 @@
+const {v4: uuidv4} = require('uuid');
+
+const socketController = (socket) => {
+    console.log('Cliente conectado', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('Cliente desconectado', socket.id);
+    });
+
+    socket.on('send-msg', (payload, callback) => {
+        // Envia el mensaje (payload) hacia el cliente
+        /* socket.broadcast.emit('send-msg', payload); */
+
+        // Retorna el id solo para el mismo cliente
+        const id = uuidv4();
+        callback(id);
+
+    })
+}
+
+module.exports = {
+    socketController
+}
